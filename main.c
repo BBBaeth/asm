097eea80d129349824b15550f036f583c73e9f4a
BBBaeth <do.road@laposte.net>
2021-09-06 16:59:12 +0200

    better handling of errors with strcpy

--- main.c
+++ main.c
@@ -3,25 +3,35 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdio.h>
+
+#define SOURCE "./libasm.h"
+#define CHARLIMIT 12
+
 int		main(int argc, char **argv)
 {
-	if (argc > 1 && argv && argv[2])
-	{
-	}
-		printf(" length of arg1 for asm : |%zd|\n", ft_strlen(argv[1]));
-		printf(" length of 'salut les potes': |%zd|\n", ft_strlen("salut les potes"));
-		if (argv[1] && argv[2])
-			printf(" arg for asm : |%d|\n", ft_strcmp(argv[1], argv[2]));
-		if (argv[1] && argv[2])
-			printf(" arg for cmp : |%d|\n", strcmp(argv[1], argv[2]));
-		errno = 0;
-		printf("avant : %d\n", errno);
-		ft_strdup(argv[1]);
-		printf("errno : %d\n", errno);
-		errno = 0;
-		if (argv[1])
-			strdup(argv[1]);
-		printf("errno : %d\n", errno);
+
+//lEN
+
+	printf("length of argv[1] for my ft_strlen : |%zd|\n", ft_strlen(argv[1]));
+	if (argv[1])
+		printf("length of arg1 for original strlen : |%zd|\n", strlen(argv[1]));
+
+//CMP
+
+	printf("cmp argv[1] argv[2] for my ft_strcmp : |%d|\n", ft_strcmp(argv[1], argv[2]));
+	if (argv[1] && argv[2])
+		printf("cmp argv[1] argv[2] for original strcmp : |%d|\n", strcmp(argv[1], argv[2]));
+
+//DUP
+
+	errno = 0;
+	printf("errno before dup of argv[1] with my ft_strdup : %d\n", errno);
+	ft_strdup(argv[1]);
+	printf("errno after dup argv[1] with my strdup : %d\n", errno);
+	errno = 0;
+	if (argv[1])
+		strdup(argv[1]);
+	printf("errno after dup with original dup : %d\n", errno);
 
 //READ
 
@@ -37,20 +47,20 @@ int		main(int argc, char **argv)
 	errno = 0;
 	r_read = read(fd, src, 4);
 	close(fd);
-	printf("read : %s, ret : %d, errno : %d\n", src, r_read, errno);
+	printf("original read : %s, ret : %d, errno : %d\n", src, r_read, errno);
 	fd = open("./Makefile", O_RDONLY);
 	errno = 0;
 	r_ft_read = ft_read(fd, dest, 4);
 	close(fd);
-	printf("ft_read : %s, ret : %d, errno : %d\n", dest, r_ft_read, errno);
+	printf("my ft_read : %s, ret : %d, errno : %d\n", dest, r_ft_read, errno);
 
 
-	fd = open("./SUPERalbatros", O_RDONLY);
+	fd = open(SOURCE, O_RDONLY);
 	errno = 0;
 	r_read = read(fd, src, 4);
 	close(fd);
 	printf("ret : %d, errno : %d\n", r_read, errno);
-	fd = open("./SUPERalbatros", O_RDONLY);
+	fd = open(SOURCE, O_RDONLY);
 	errno = 0;
 	r_ft_read = ft_read(fd, dest, 4);
 	close(fd);
@@ -59,25 +69,42 @@ int		main(int argc, char **argv)
 // WRITE
 
 	errno = 0;
-	printf("write : %ld", write(1, "hello!\n", 5));
+	printf(" :: write (number of bytes written) : '%ld'", write(1, "hello!\n", 5));
 	printf(", errno : %d\n", errno);
 	errno = 0;
-	printf("ft_write : %ld", ft_write(1, "hello!\n", 5));
+	printf(" :: ft_write (number of bytes written): '%ld'", ft_write(1, "hello!\n", 5));
 	printf(", errno : %d\n", errno);
 
 	errno = 0;
-	printf("write : %ld", write(1, argv[1], 12));
+	printf(" :: write (argv[1] capped at CHARLIMIT) : %ld", write(1, argv[1], CHARLIMIT));
 	printf(", errno : %d\n", errno);
 	errno = 0;
-	printf("ft_write : %ld", ft_write(1, argv[1], 12));
+	printf(" :: ft_write (argv[1] capped at CHARLIMIT) : %ld", ft_write(1, argv[1], CHARLIMIT));
 	printf(", errno : %d\n", errno);
 
 	errno = 0;
-	printf("write : %ld", write(1, argv[2], -10));
+	printf("write (argv[2] capped at negative) : %ld", write(1, argv[2], -10));
 	printf(", errno : %d\n", errno);
 	errno = 0;
-	printf("ft_write : %ld", ft_write(1, argv[2], -10));
+	printf("ft_write (argv[2] capped at negative) : %ld", ft_write(1, argv[2], -10));
 	printf(", errno : %d\n", errno);
 
+// COPY
+
+	char mystring[] = "hello world";
+	char mycopy[] = "hi";
+	char mycopy2[] = "I love sushi and cute little puppies.";
+	char empty[] = "";
+	char longstring[] = "blablablablablabla";
+
+	printf("original strcpy (test1) : |%s|\n", strcpy(mystring, mycopy));
+	printf("my ft_strcpy    (test1) : |%s|\n", ft_strcpy(mystring, mycopy));
+	printf("original strcpy (test2) : |%s|\n", strcpy(mycopy2, empty));
+	printf("my ft_strcpy    (test2) : |%s|\n", ft_strcpy(mycopy2, empty));
+//	printf("original strcpy (test3) : |%s|\n", strcpy(empty, longstring));
+	printf("my ft_strcpy    (test3) : |%s|\n", ft_strcpy(empty, longstring));
+	if (argv[1] && argv[2])
+		printf("original strcpy (argv[1], argv[2]) : |%s|\n", strcpy(argv[1], argv[2]));
+	printf("my ft_strcpy    (argv[1], argv[2]) : |%s|\n", ft_strcpy(argv[1], argv[2]));
 	return (0);
 }
--- srcs/ft_strcpy.s
+++ srcs/ft_strcpy.s
@@ -6,26 +6,36 @@ section .data
 
 section .text
 
-_error_no_arg:          ; strcpy would segfault
-	mov rax, 0
+_arg_error:             ; original strcpy would have undefined behaviour
+
+	mov BYTE rax, 0     ; my strcpy returns a null string
 	ret
 
 _exit:
+
 	mov rax, rdi        ; strcpy returns s1
 	ret                 ; return rax <- rdi (ptr to s1)
 
 _strcpy_loop:
+
 	inc rax             ; rax++
+
 	mov cl, [rsi + rax] ; cl equivalent to al but in CX register
 	mov [rdi + rax], cl ; dst[ptr] = src[ptr]
+
 	cmp cl, byte 0      ; is char == \0 ?
 	je _exit            ; finished copying
 	jmp _strcpy_loop
 
 _ft_strcpy: ; rsi = source, rdi = dest
-	mov rax, -1  ; rax is our ptr
-;	cmp rdi, byte 0 ; does dst point somewhere ? if not nothing to cpy
-;	jng _error_no_arg
-;	cmp rsi, byte 0 ; does src exist ? if not then nothing to cpy : return null
-;	jng _error_no_arg
+
+	mov rax, -1         ; rax is our ptr
+
+	; error cases where dest < source or dest or source do not exist
+
+	cmp rdi, 0          ; does argv[1] exist ?
+	jng _arg_error
+	cmp rsi, 0          ; does argv[2] exist ?
+	jng _arg_error
+
 	jmp _strcpy_loop
